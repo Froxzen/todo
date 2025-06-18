28b7260a84588cb6514d6eda0ad2afefce7aba8d
Froxzen <harvardchong1@gmail.com>
2025-06-18 23:50:16 +1000

    Enhance ListGroup component to handle item clicks and improve code formatting

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,19 +1,26 @@
-function ListGroup() {
-	let items = [
-		'Sydney',
-		'Perth',
-		'Melbourne',
-		'Canberra',
-		'Brisbane'
-	]
+import type { MouseEvent } from "react";
 
+function ListGroup() {
+	let items = ["Sydney", "Perth", "Melbourne", "Canberra", "Brisbane"];
+
+	// Event Handler
+	const handleClick = (event: MouseEvent) => console.log(event);
 
 	return (
 		<>
 			<h1>List</h1>
+
 			{items.length === 0 && <p>No items found</p>}
 			<ul className="list-group">
-			{items.map(item => <li key={item} className="list-group-item">{item}</li>)}
+				{items.map((item, index) => (
+					<li
+						key={item}
+						className="list-group-item"
+						onClick={handleClick}
+					>
+						{item}
+					</li>
+				))}
 			</ul>
 		</>
 	);
