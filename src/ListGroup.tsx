5990a65f3e19220d1e690fc1e6b590a26bb18fcf
Froxzen <harvardchong1@gmail.com>
2025-06-18 23:37:58 +1000

    Refactor ListGroup component to use dynamic items array and improve rendering

--- src/ListGroup.tsx
+++ src/ListGroup.tsx
@@ -1,12 +1,21 @@
 function ListGroup() {
+	let items = [
+		'Sydney',
+		'Perth',
+		'Melbourne',
+		'Canberra',
+		'Brisbane'
+	]
+
+
 	return (
-		<ul className="list-group">
-			<li className="list-group-item">An item</li>
-			<li className="list-group-item">A second item</li>
-			<li className="list-group-item">A third item</li>
-			<li className="list-group-item">A fourth item</li>
-			<li className="list-group-item">And a fifth one</li>
-		</ul>
+		<>
+			<h1>List</h1>
+			{items.length === 0 && <p>No items found</p>}
+			<ul className="list-group">
+			{items.map(item => <li key={item} className="list-group-item">{item}</li>)}
+			</ul>
+		</>
 	);
 }
 
